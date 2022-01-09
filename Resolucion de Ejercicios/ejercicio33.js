51e693c075bc5a0825a88305fe48f48f70b79ddd
Andy <ataipe@unitru.edu.pe>
2022-01-08 23:55:38 -0500

    actualizando

--- Resolucion de Ejercicios/ejercicio33.js
+++ Resolucion de Ejercicios/ejercicio33.js
@@ -1,8 +1,9 @@
 // Realizar un programa que indique si una palabra es capicúa ejemplo: oso, radar, oro, solos
 
+let palabra = prompt("Ingrese palabra: ");
+let capicua = (palabra === palabra.split("").reverse().join(""))?'si son capicuas':'no es capicua';
 
-
-
+console.log(capicua);
 
 
 
