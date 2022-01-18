1ff745854024e48c9b54fea4faf489d3e478b595
Jose Angel Silva Cruz <joseangel413silva@gmail.com>
2022-01-17 23:38:51 -0600

    se agrega los comentarios de los primeros dos ejercicios. 

--- eje2.py
+++ eje2.py
@@ -1,2 +1,11 @@
-mensaje = "Hola Mundo"
-print(mensaje)
+# Eje 2
+
+# Se crea una nueva variable que se le asigna la variable tipo cadena 
+# por lo que el mensaje debe que estar dentro de comillas
+
+mensaje = "¡Hola Mundo!" # Declaracion de la variable tipo string
+
+print(mensaje) # se usa la funcion print dando que se va imprimir la info que
+                # tiene la variable mensaje
+                
+# fin del programa
--- holamundo.py
+++ holamundo.py
@@ -1,1 +1,6 @@
-print("Hola mundo")
+# Eje 1: Hola mundo
+# Se manda a llamar la funcion print para imprimir en la consola dentro de los
+# parentesis con comillas dobles se escribe el mensaje a imprimir
+
+print("Hola mundo") # Se usa la funcion print para imprimir el mensaje
+# Fin del programa
